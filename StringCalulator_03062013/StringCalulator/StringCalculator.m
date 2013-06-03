da0ce389b590b49acf32fa5efe6ce4080eda3d77
hunglv <hunglv@qsoftvietnam.com>
2013-06-03 13:56:28 +0700

    green + clean

--- StringCalulator_03062013/StringCalulator/StringCalculator.m
+++ StringCalulator_03062013/StringCalulator/StringCalculator.m
@@ -14,11 +14,8 @@
     NSInteger result = 0;
     if (addedString.length > 0) {
         NSArray *subStrings = [addedString componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@","]];
-        if (subStrings.count == 1) {
-            result = [addedString integerValue];
-        }
-        if (subStrings.count == 2) {
-            result = [[subStrings objectAtIndex:0] integerValue] + [[subStrings objectAtIndex:1] integerValue];
+        for (NSString *subString in subStrings) {
+            result += [subString integerValue];
         }
     }
     return result;
