74bd2b2c5c9dd30da89475082b70518644b808dc
hunglv <hunglv@qsoftvietnam.com>
2013-06-05 13:48:35 +0700

    Start

---
+++ StringCalulator_05062013/StringCalulator/StringCalculator.m
@@ -0,0 +1,13 @@
+//
+//  StringCalculator.m
+//  StringCalulator
+//
+//  Created by El Nino on 5/27/13.
+//  Copyright (c) 2013 El Nino. All rights reserved.
+//
+
+#import "StringCalculator.h"
+
+@implementation StringCalculator
+
+@end
---
+++ StringCalulator_05062013/StringCalulatorTests/StringCalculatorTest.m
@@ -0,0 +1,33 @@
+//
+//  StringCalculatorTest.m
+//  StringCalulator
+//
+//  Created by El Nino on 5/27/13.
+//  Copyright (c) 2013 El Nino. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "StringCalculator.h"
+
+SPEC_BEGIN(StringCalculatorTest)
+describe(@"StringCalculator test", ^{
+    __block StringCalculator *_instance = nil;
+    
+    beforeAll(^{
+        
+    });
+    
+    afterAll(^{
+        _instance = nil;
+    });
+    
+    beforeEach(^{
+        
+    });
+    
+    afterEach(^{
+        _instance = nil;
+    });
+    
+});
+SPEC_END
