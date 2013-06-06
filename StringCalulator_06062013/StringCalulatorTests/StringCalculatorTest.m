487647f79ad964e95ea29be0f412c578d15c62de
hunglv <hunglv@qsoftvietnam.com>
2013-06-06 13:36:17 +0700

    start

---
+++ StringCalulator_06062013/StringCalulatorTests/StringCalculatorTest.m
@@ -0,0 +1,16 @@
+//
+//  StringCalculatorTest.m
+//  StringCalulator
+//
+//  Created by El Nino on 5/27/13.
+//  Copyright (c) 2013 El Nino. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "StringCalculator.h"
+
+SPEC_BEGIN(StringCalculatorTest)
+describe(@"StringCalculator test", ^{
+    
+});
+SPEC_END
