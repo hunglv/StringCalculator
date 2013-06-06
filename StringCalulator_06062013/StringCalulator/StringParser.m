04a77ee320826f41fe25716993f4070c0958b370
hunglv <hunglv@qsoftvietnam.com>
2013-06-06 14:27:31 +0700

    clean

--- StringCalulator_06062013/StringCalulator/StringParser.m
+++ StringCalulator_06062013/StringCalulator/StringParser.m
@@ -11,11 +11,12 @@
 @implementation StringParser
 
 -(NSArray *)stringParser:(NSString *)string {
-    NSMutableArray *result = [[NSMutableArray alloc] init];;
+    NSMutableArray *result = [[NSMutableArray alloc] init];
     if (string.length == 0) {
         [result addObject:string];
     } else {
-        NSArray *subArray = [string componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@",\n"]];
+        NSMutableString *delimiterString = [[NSMutableString alloc] initWithFormat:@",\n"];
+        NSArray *subArray = [string componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:delimiterString]];
         for (NSString *numString in subArray) {
             if ([numString integerValue] != 0) {
                 [result addObject:numString];
